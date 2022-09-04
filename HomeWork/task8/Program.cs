e44d307d501eed83638c88c10aaf07e058dac2e5
Алексей Машков <AlexeyMashkoff@yandex.ru>
2022-09-04 21:13:25 +0300

    Realized Z8

---
+++ HomeWork/task8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+int i=1;
+Console.WriteLine("Введите число N и нажмите ENTER:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while(i <= number)
+{
+    if (i%2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+i++;
+}
+
+
