f202414befdaed82a9b94ac6fb6b94763e120361
Алексей Машков <AlexeyMashkoff@yandex.ru>
2022-09-04 16:28:11 +0300

    realized Z4

---
+++ HomeWork/task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход 
+// три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите число a и нажмите Enter:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b и нажмите Enter:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число c и нажмите Enter:");
+int c = Convert.ToInt32(Console.ReadLine());
+int max_number = 0;
+if (a > max_number) max_number = a;
+if (b > max_number) max_number = b;
+if (c > max_number) max_number = c;
+Console.WriteLine("Максимальное число "+max_number+"");
