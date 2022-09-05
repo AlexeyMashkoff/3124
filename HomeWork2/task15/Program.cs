e58766c2c70a24b0fb0fae54db99f4a6e755609a
AlexeyMashkoff <112006775+AlexeyMashkoff@users.noreply.github.com>
2022-09-05 11:37:58 +0300

    Add files via upload

---
+++ HomeWork2/task15/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write(" Введите цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void TheWeek (int dayNumber) 
+{
+  if (dayNumber == 6 || dayNumber == 7) 
+  {
+  Console.WriteLine(" да - ВЫХОДНОЙ ");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) 
+  {
+    Console.WriteLine(" неправильное значение дня недели ");
+  }
+  else Console.WriteLine(" нет - иди работай(^^) ");
+}
+
+TheWeek(dayNumber);
