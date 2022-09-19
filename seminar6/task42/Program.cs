a0515a00dcb9f262fbc88049a1c66b1508fd3559
Алексей Машков <AlexeyMashkoff@yandex.ru>
2022-09-20 00:25:27 +0300

    added

---
+++ seminar6/task42/Program.cs
@@ -0,0 +1,20 @@
+﻿
+// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintBinaryView(int number)
+{
+    if(number <= 0) 
+    {
+        return;
+    }
+    PrintBinaryView(number / 2);
+    Console.Write(number % 2);
+}
+
+int num = ReadNumber("Введите число:");
+PrintBinaryView(num);
