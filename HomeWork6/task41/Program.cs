8965b8096e3090b5762a897ba9675a4804af13c6
Алексей Машков <AlexeyMashkoff@yandex.ru>
2022-09-21 01:19:23 +0300

    Real

---
+++ HomeWork6/task41/Program.cs
@@ -0,0 +1,34 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+// Ввод чисел останавливается при помощи ввода стоп-слова "stop"
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+string ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Console.ReadLine() ?? "";
+}
+
+int count = 0;
+{
+while(true)
+{
+    string answer = ReadNumber("Введите число и нажмите Enter, для остановки введите слово - stop");
+    if (answer == "stop")
+    {
+        break;
+    }
+    else
+    {
+        int number = Convert.ToInt32(answer);
+        if(number > 0)
+        {
+            count++;
+        }
+    }
+}
+}
+Console.WriteLine(count);
